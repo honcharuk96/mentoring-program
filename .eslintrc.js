255c95a9dd21c43fe90b9119df85df2dfd23c6a5
Denys Honcharuk <honcharuk.dynamo@gmail.com>
2021-12-03 13:40:46 +0200

    add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,41 @@
 module.exports = {
-    env: {
-        browser: true,
-        node: true,
-        es2021: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:storybook/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-    ],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: 'module',
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
     },
-    plugins: [
-        'react',
-        'react-hooks',
-    ],
-    settings: {
-        react: {
-            pragma: 'React',
-            version: 'detect',
-        },
-    },
-    rules: {
-        quotes: ['error', 'single', { avoidEscape: true }],
-        'comma-dangle': ['error', 'always-multiline'],
-        'arrow-body-style': ['error', 'as-needed'],
-        'global-require': 'off',
-        'class-methods-use-this': 'off',
-        'import/no-dynamic-require': 'off',
-        'import/prefer-default-export': 'off',
-        'react/destructuring-assignment': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        'react/state-in-constructor': ['error', 'always'],
-        'jsx-a11y/label-has-for': 'off',
-        'jsx-a11y/label-has-associated-control': 'off',
-        'react/no-array-index-key': 'off',
-        'react-hooks/rules-of-hooks': 'error',
-        'react/no-unescaped-entities': 'off',
-    },
+  },
+  rules: {
+    quotes: ['error', 'single', {
+      avoidEscape: true,
+    }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'global-require': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-dynamic-require': 'off',
+    'import/prefer-default-export': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/state-in-constructor': ['error', 'always'],
+    'jsx-a11y/label-has-for': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react/no-array-index-key': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/no-unescaped-entities': 'off',
+  },
 };
