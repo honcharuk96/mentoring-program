0c253f8fa08b92a9e5fd28c4f99a78f0ef6773d2
Denys Honcharuk <honcharuk.dynamo@gmail.com>
2021-12-04 07:50:18 +0200

    add snapshots

---
+++ src/header/searchForm/__tests__/searchForm.component.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ShallowRenderer from 'react-test-renderer/shallow';
+import SearchForm from '../searchForm.component';
+
+const shallow = new ShallowRenderer();
+const setPosterDataFromForm = jest.fn()
+
+
+describe('<SearchForm />', () => {
+
+    it('should render SearchForm withoutPosterData', () => {
+        const header =  shallow.render(<SearchForm setPosterDataFromForm={setPosterDataFromForm}/>);
+        expect(header).toMatchSnapshot();
+    });
+});
